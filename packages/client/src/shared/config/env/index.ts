0c6796d8a9fa2883bb6bcf361e0a0f6e341f23b8
Дмитрий Пропастин <dima.propastin@gmail.com>
2024-11-13 16:14:26 +0300

    fix errors

--- packages/client/src/shared/config/env/index.ts
+++ packages/client/src/shared/config/env/index.ts
@@ -7,12 +7,9 @@
 //     : __EXTERNAL_SERVER_URL__;
 // export const PUBLIC_VAPID_KEY = __PUBLIC_VAPID_KEY__;
 
-export const SERVER_API_URL = 'http://localhost:3001';
+export const SERVER_API_URL = 'https://memory-cards.ya-praktikum.tech:3001';
 export const YANDEX_API_URL = 'https://ya-praktikum.tech/api/v2';
 export const YANDEX_RESOURCES_URL =
   'https://ya-praktikum.tech/api/v2/resources';
-export const SERVER_HOST =
-  typeof window === 'undefined'
-    ? 'http://server:3000'
-    : 'http://localhost:3000';
+export const SERVER_HOST = 'https://memory-cards.ya-praktikum.tech';
 export const PUBLIC_VAPID_KEY = 'piU0W5nSUdjz5MxsJIdW2JlxqizQHMNV9NjLPExrzvI';
